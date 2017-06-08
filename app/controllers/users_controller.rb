ba22ceda3f352d02ae595d2c94c249e09feb41ab
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-08 15:00:50 +0200

    check if name and password are correct

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
 
   def login
   end
+
+  def check
+    @current_user = User.where(name: params[:name], password: params[:password]).first
+    if @current_user
+      flash[:info] = "Welcome #{@current_user.name} !"
+      redirect_to "/users/home"
+    else
+      flash[:info] = "Sadly, we don't know you yet..."
+      redirect_to "/users/login"
+    end
+  end
+  
 end
