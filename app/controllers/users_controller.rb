678275102d7d46bdbd0038ec3066dd70fcc123c8
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-08 15:16:35 +0200

    setting a set_current_user function to be called in Home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def home
+    set_current_user
   end
 
   def login
@@ -8,12 +9,21 @@ class UsersController < ApplicationController
   def check
     @current_user = User.where(name: params[:name], password: params[:password]).first
     if @current_user
+      session[:user_id] = @current_user.id
       flash[:info] = "Welcome #{@current_user.name} !"
       redirect_to "/users/home"
     else
+      session[:user_id] = nil
       flash[:info] = "Sadly, we don't know you yet..."
       redirect_to "/users/login"
     end
+  end
+
+private 
+
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
   end
   
 end
