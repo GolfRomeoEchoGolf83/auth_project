bf3daff97700627ce69072353fb57f45f13d0aea
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-08 15:02:17 +0200

    setting home-login-check routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/home'
-
-  get 'users/login'
+  get 'users/home' => 'users#home'
+  get 'users/login'=> 'users#login'
+  post 'users/login' => 'users#check'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
